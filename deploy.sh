7e4abad6a3d8cd478c2d0b4dcfa0261a64db3e81
Mich <mr4209@nyu.edu>
2015-08-10 17:28:43 -0400

    change script to bash

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
-# @author Joschi <josua.krause@gmail.com>
-# created 2015-02-16 15:35
+#!/bin/bash
 
-if [ -z `git branch | grep "* master"` ]; then
-  echo "not on master"
-  exit 1
+if [ -n `git branch | grep "* master"` ]
+then
+    git checkout gh-pages && git merge --ff-only master && git push origin gh-pages && git checkout master
+else
+    echo "Switch to master"
+    exit 1
 fi
 
-git checkout gh-pages && git merge --ff-only master && git push origin gh-pages && git checkout master
