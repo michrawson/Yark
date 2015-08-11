6ef71c180637093fdc69d3a1c307caabec39fab4
arieldexler <bad225@nyu.edu>
2015-08-10 22:28:42 -0400

    Update mapper.java

--- ProjectA/mapper.java
+++ ProjectA/mapper.java
@@ -11,21 +11,17 @@ extends Mapper<LongWritable, Text, Text, Text> {
 public void map(LongWritable key, Text value, Context context)
 throws IOException, InterruptedException {
 
-
 String str = value.toString().replace("\"","");
 String[] flds  = str.split(",");     
- if (! flds[0].equals("Source Country") && flds.length==10757)  
- {
-int index =0;
-for (int i=0;i<5;i++) {
-      index = str.indexOf(",",index+1);
-       }
-String val = str.substring(index+1); // get the measures only
-   
-  context.write(new Text(String.valueOf(flds[2]) +  "_"+flds[3]+"_"+flds[4] ),    new Text(val));
-  }
-
-
+if (! flds[0].equals("Source Country") && flds.length==10757)  
+{
+   int index =0;
+   for (int i=0;i<5;i++) {
+         index = str.indexOf(",",index+1);
+     }
+   String val = str.substring(index+1); // get the measures only
+   context.write(new Text(String.valueOf(flds[2]) +  "_"+flds[3]+"_"+flds[4] ),    new Text(val));
+}
                                           }
                                                }
 
