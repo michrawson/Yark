c19c7ca3b874c492856a826e5d3626e4cf84b39f
Mich <mr4209@nyu.edu>
2015-08-10 22:03:17 -0400

    Commit for Ariel

--- ProjectA/crisis.java
+++ ProjectA/crisis.java
@@ -4,10 +4,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
 public class crisis {
 public static void main(String[] args) throws Exception {
 if (args.length != 2) {
-System.err.println("Usage: worccount <input path> <output path>");
+System.err.println("Usage: crisis <input path> <output path>");
 System.exit(-1);
 }
 Job job = new Job();
@@ -18,7 +19,7 @@ FileOutputFormat.setOutputPath(job, new Path(args[1]));
 job.setMapperClass(mapper.class);
 job.setReducerClass(reducer.class);
 job.setOutputKeyClass(Text.class);
-job.setOutputValueClass(DoubleWritable.class);
+job.setOutputValueClass(Text.class);
 System.exit(job.waitForCompletion(true) ? 0 : 1);
 }
 }
